739a4a219cb3dde4d1ee77ebfc33bbfb7fe6b5df
javier  bolanos <javierbolanos@MacBook-Air-de-javier.local>
2020-02-12 10:21:24 -0600

    guardar datos de form alumno en loca

---
+++ app.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded",(event) => {
+
+    const formAlumno = document.querySelector("#frmalumno");
+    formAlumno.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        let codigo = document.querySelector["#txtcodigoalumno"].value,
+        nombre = document.querySelector[""]
+
+    });
+
+});
+
