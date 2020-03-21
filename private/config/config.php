0e00d7fb84daeb20862ab267c0690ff3462fdcae
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-03-21 09:02:43 -0600

    app_academica

--- private/config/config.php
+++ private/config/config.php
@@ -1,4 +1,6 @@
 <?php
-include('../conexion/DB.php');
-$conexion = new Conexion('localhost','root','','db_academica');
+
+include('../../Conexion/DB.php');
+$conexion = new Conexion('localhost','root','','db_app_academica');
+
 ?>
