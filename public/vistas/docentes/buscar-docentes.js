51dad9a00d73c847f78c4c3c61f2f6d04a018872
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-04-03 18:47:01 -0600

    app_academica

--- public/vistas/docentes/buscar-docentes.js
+++ public/vistas/docentes/buscar-docentes.js
@@ -1,26 +1,47 @@
-var appBuscarDocentes = new Vue({
-    el:'#frm-buscar-docentes',
-    data:{
-        misdocentes:[],
-        valor:''
-    },
-    methods:{
-        buscarDocente:function(){
-            fetch(`private/Modulos/docentes/procesodoce.php?proceso=buscarAlumno&alumno=${this.valor}`).then(resp=>resp.json()).then(resp=>{
-                this.misdocentes = resp;
+export function modulo(){
+    var $ = el => document.querySelector(el),
+        frmBuscarAlumnos = $("#txtBuscarDocente");
+    frmBuscarAlumnos.addEventListener('keyup', e=>{
+        traerDatos(frmBuscarAlumnos.value);
+    });
+    let modificarAlumno = (alumno)=>{
+        $("#frm-docentes").dataset.accion = 'modificar';
+        $("#frm-docentes").dataset.iddocente = alumno.idDocente;
+        $("#txtCodigoDocente").value = alumno.codigo;
+        $("#txtNombreDocente").value = alumno.nombre;
+        $("#txtDireccionDocente").value = alumno.direccion;
+        $("#txtNitDocente").value = alumno.nit;
+    };
+    let eliminarAlumno = (idDocente)=>{
+        fetch(`private/Modulos/docentes/procesodoce.php?proceso=eliminarAlumno&alumno=${idDocente}`).then(resp=>resp.json()).then(resp=>{
+            traerDatos('');
+        });
+    };
+    let traerDatos = (valor)=>{
+        fetch(`private/Modulos/docentes/procesodoce.php?proceso=buscarAlumno&alumno=${valor}`).then(resp=>resp.json()).then(resp=>{
+            let filas = '';
+            resp.forEach(alumno => {
+                filas += `
+                    <tr data-iddocente='${alumno.idDocente}' data-alumno='${JSON.stringify(alumno)}'>
+                        <td>${alumno.codigo}</td>
+                        <td>${alumno.nombre}</td>
+                        <td>${alumno.direccion}</td>
+                        <td>${alumno.nit}</td>
+                        <td>
+                            <input type="button" class="btn btn-outline-danger text-white" value="del">
+                        </td>
+                    </tr>
+                `;
             });
-        },
-        modificarAlumno:function(docente){
-            appdocente.docente = docente;
-            appdocente.docente.accion = 'modificar';
-        },
-        eliminarAlumno:function(idDocente){
-            fetch(`private/Modulos/docentes/procesodoce.php?proceso=eliminarAlumno&alumno=${idDocente}`).then(resp=>resp.json()).then(resp=>{
-                this.buscarDocente();
+            $("#tbl-buscar-docentes > tbody").innerHTML = filas;
+            $("#tbl-buscar-docentes > tbody").addEventListener("click",e=>{
+                if( e.srcElement.parentNode.dataset.alumno==null ){
+                    eliminarAlumno( e.srcElement.parentNode.parentNode.dataset.iddocente );
+                } else {
+                    modificarAlumno( JSON.parse(e.srcElement.parentNode.dataset.alumno) );
+                }
             });
-        }
-    },
-    created:function(){
-        this.buscarAlumno();
-    }
-});
+        });
+    };
+    traerDatos('');
+}
