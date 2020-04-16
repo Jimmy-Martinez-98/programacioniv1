06067546c1cf263d9113cf689021858cea74b4ca
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-04-16 15:09:06 -0600

    pf

--- public/vistas/login/login.js
+++ public/vistas/login/login.js
@@ -1,29 +1,8 @@
+$(document).ready(function () {
 
-var appdocente = new Vue({
-    el:'#frm-docentes',
-    data:{
-        docente:{
-            idDocente  : 0,
-            accion    : 'nuevo',
-            codigo    : '',
-            nombre    : '',
-            direccion : '',
-            nit       : '',
-            msg       : ''
-        }
-    },
-    methods:{
-        guardarDocente:function(){
-            fetch(`private/Modulos/docentes/procesodoce.php?proceso=recibirDatos&alumno=${JSON.stringify(this.docente)}`).then( resp=>resp.json() ).then(resp=>{
-                this.docente.msg = resp.msg;
-                this.docente.idAlumno = 0;
-                this.docente.codigo = '';
-                this.docente.nombre = '';
-                this.docente.direccion = '';
-                this.docente.nit = '';
-                this.docente.accion = 'nuevo';
-                appBuscarDocentes.buscarDocente();
-            });
-        }
-    }
+	$('.registro').click(function (e) { 
+		e.preventDefault();
+		console.log('gfaasd');
+		$('.container').load('public/vistas/login/registrar.html');
+	});
 });
